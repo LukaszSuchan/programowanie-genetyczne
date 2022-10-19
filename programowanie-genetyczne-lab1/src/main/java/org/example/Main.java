a1ea6efc52b61ec2bdd448c23e248cf1dad63dc4
Lukasz Suchan <lukasz.suchan@interia.eu>
2022-10-19 23:46:40 +0200

    add resource directory

--- programowanie-genetyczne-lab1/src/main/java/org/example/Main.java
+++ programowanie-genetyczne-lab1/src/main/java/org/example/Main.java
@@ -19,12 +19,12 @@ public class Main
                 -PI/2,
                 PI/2,
                 0.01,
-                "function1.dat"
+                "data/function1.dat"
         );
 
-//        DataCreator.createDataFotOneParam(new FunctionOne(request, 7));
-//        TinyGpExtended tinyGpExtended = new TinyGpExtended("src/main/resources/function1.dat", -1);
-//        tinyGpExtended.evolve();
+        DataCreator.createDataFotOneParam(new FunctionOne(request, 7));
+        TinyGpExtended tinyGpExtended = new TinyGpExtended("src/main/resources/data/function1.dat", -1);
+        tinyGpExtended.evolve();
 
         var function = "( COS(  COS( ( COS( 5.914799065538958) + -4.122307634293506))) + (((-45.50275332686009 / -41.93761879577522) +  SIN( X1 )) + ( COS( -25.63420283763658) +  SIN( ( SIN( ( SIN( (-45.50275332686009 + (37.98279082013164 + -38.1255508021896))) - ( COS( -52.02533614068236) * (-20.96334258997463 / (-55.96684585728706 - ((-85.20443248080582 * 89.90343033775352) - ((54.930656106835585 / 18.228231637292566) - -29.457774969341372))))))) - -55.447395218894634)))))\n";
         ExcelConverter excelConverter = new ExcelConverter();
