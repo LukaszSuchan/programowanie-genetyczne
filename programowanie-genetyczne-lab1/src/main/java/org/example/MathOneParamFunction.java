c0ff71615f5cf008ed44454f6e68482108242bc1
LukaszSuchan <72552687+LukaszSuchan@users.noreply.github.com>
2022-10-19 21:01:32 +0200

    Delete programowanie-genetyczne-lab1 directory

--- programowanie-genetyczne-lab1/src/main/java/org/example/MathOneParamFunction.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-import org.example.model.DataCreatorRequestDto;
-
-public  interface MathOneParamFunction {
-    double call(double x);
-    DataCreatorRequestDto getRequest();
-}
